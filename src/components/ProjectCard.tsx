6de6e164c63488619f717e139450d466aa1309a6
René Lázaro Collado Arteaga <94305086+critBus@users.noreply.github.com>
2025-03-21 12:05:52 -0400

    demos y mas info

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import Link from 'next/link';
 import TechBadge from './TechBadge';
+import { FaGooglePlay, FaGlobe } from 'react-icons/fa';
+
+interface DemoLink {
+  type: string;
+  url: string;
+}
 
 interface ProjectCardProps {
   name: string;
   description: string[];
   technologies: string[];
   links?: string[];
-  demo?: string;
+  demo?: DemoLink[];
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ name, description, technologies, links, demo }) => {
+  const getDemoIcon = (type: string) => {
+    switch (type) {
+      case 'Google Play Store':
+        return <FaGooglePlay className="inline mr-1" />;
+      case 'Web':
+        return <FaGlobe className="inline mr-1" />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="bg-white dark:bg-gray-800 shadow-md rounded-lg p-6">
       <h3 className="text-xl font-bold mb-2">{name}</h3>
@@ -31,7 +48,12 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ name, description, technologi
             {`Repositorio ${links.length > 1 ? (index + 1) : ''}`}
           </a>
         ))}
-        {demo && <a href={demo} className="text-blue-500 hover:underline">Demo</a>}
+        {demo && demo.map((demoLink, index) => (
+          <a key={index} href={demoLink.url} className="text-blue-500 hover:underline flex items-center">
+            {getDemoIcon(demoLink.type)}
+            {demoLink.type}
+          </a>
+        ))}
       </div>
     </div>
   );
